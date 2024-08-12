af76c3eb6c2af1eecd26a8f3fd219276e8c5dfe1
kimhyunjin <khjzzm@gmail.com>
2024-08-12 17:58:39 +0900

    Add Toss Payments integration
    Implemented Toss Payments API integration including controllers, response handling, and HTML templates for success and failure pages. Added styles for the user interface.

---
+++ src/main/kotlin/com/example/paymentservice/payment/adapter/out/web/executor/TossPaymentExecutor.kt
@@ -0,0 +1,27 @@
+package com.example.paymentservice.payment.adapter.out.web.executor
+
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Mono
+
+@Component
+class TossPaymentExecutor (
+    private val tossPaymentClient: WebClient,
+    private val uri : String = "/v1/payments/confirm"
+){
+
+    fun execute(paymentKey:String, orderId: String, amount: String): Mono<String>{
+        return tossPaymentClient.post()
+            .uri(uri)
+            .bodyValue("""
+                {
+                    "paymentKey": "$paymentKey",
+                    "orderId": "$orderId",
+                    "amount": $amount
+                }
+            """.trimIndent())
+            .retrieve()
+            .bodyToMono(String::class.java)
+    }
+
+}
